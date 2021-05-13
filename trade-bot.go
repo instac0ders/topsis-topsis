6cc24628ac3280907cb11efceac5bdb5bb0b0895
instac0ders <91621621+instac0ders@users.noreply.github.com>
2021-05-13 09:34:38 +0000

    Update return-es6.js

--- trade-bot.go
+++ trade-bot.go
@@ -11,4 +11,4 @@ import (
 var config, err = toml.LoadFile("trade-bot.conf")
 var configTree = config.Get("config").(*toml.Tree)
 var base_coin = configTree.Get("base_coin").(string)
-var market_c
+var market_coin = c
