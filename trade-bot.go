a65f6d40620a569ae4ca9e191302a55492b65a05
instac0ders <91621621+instac0ders@users.noreply.github.com>
2021-07-26 09:29:22 +0000

    work on documentation

--- trade-bot.go
+++ trade-bot.go
@@ -14,4 +14,4 @@ var base_coin = configTree.Get("base_coin").(string)
 var market_coin = configTree.Get("market_coin").(string)
 var api_version = configTree.Get("api_version").(string)
 var base_url = configTree.Get("base_url").(string)
-var api_key = configTree.Get("api_k
+var api_key = configTree.Get("api_key"
