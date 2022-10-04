fc15b6a78749f06a79141010f9e28ba680183e78
instac0ders <91621621+instac0ders@users.noreply.github.com>
2022-10-04 17:02:27 +0000

    release 0.4

--- trade-bot.go
+++ trade-bot.go
@@ -44,4 +44,4 @@ func last_price(t time.Time) {
 	ticker, _ := bittrex_client.GetTicker(base_coin + "-" + market_coin)
 	last_sell := decimal.NewFromFloat(last_sell)
 	last_bid := ticker.Bid
-	net_diff := fmt.Sprint(((last_bid.Sub(last_sell)).Div(last_sell)).Mul
+	net_diff := fmt.Sprint(((last_bid.Sub(last_sell)).Div(last_sell)).Mul(decim
