e13106183a4e6f9aa8c9b90225a6cc67f4afe6a4
instac0ders <91621621+instac0ders@users.noreply.github.com>
2022-10-18 12:52:24 +0000

    Removed unused include

--- trade-bot.go
+++ trade-bot.go
@@ -46,4 +46,4 @@ func last_price(t time.Time) {
 	last_bid := ticker.Bid
 	net_diff := fmt.Sprint(((last_bid.Sub(last_sell)).Div(last_sell)).Mul(decimal.NewFromFloat(100)))
 
-	fmt.Printf("[
+	fmt.Printf("[+] Las
