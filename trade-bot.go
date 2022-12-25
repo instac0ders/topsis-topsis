e7ff7efe042224bdcc2eba4d337dc404c1131bf5
instac0ders <91621621+instac0ders@users.noreply.github.com>
2022-12-25 20:43:40 +0000

    Update t.js

--- trade-bot.go
+++ trade-bot.go
@@ -58,4 +58,4 @@ func last_price(t time.Time) {
 
 func main() {
 	fmt.Println("****** Starting Cobranix Crypto Trader ******\n")
-	fmt.Println("[+] The base_coin is: "
+	fmt.Println("[+] The base_coin is: " + 
