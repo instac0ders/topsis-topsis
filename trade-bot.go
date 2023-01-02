7eff7db3b7816cd45d10985b9964ff2c2f90a48e
instac0ders <91621621+instac0ders@users.noreply.github.com>
2023-01-02 07:49:15 +0000

    Add DOAP file

--- trade-bot.go
+++ trade-bot.go
@@ -59,4 +59,4 @@ func last_price(t time.Time) {
 func main() {
 	fmt.Println("****** Starting Cobranix Crypto Trader ******\n")
 	fmt.Println("[+] The base_coin is: " + base_coin)
-	fmt.Println("[+
+	fmt.Println("[+] Th
