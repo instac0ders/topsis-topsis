e931e43aa20790c92872e1a8a94bf6bf0b9db981
instac0ders <91621621+instac0ders@users.noreply.github.com>
2023-02-04 12:34:01 +0000

    travis configuration

--- trade-bot.go
+++ trade-bot.go
@@ -60,4 +60,5 @@ func main() {
 	fmt.Println("****** Starting Cobranix Crypto Trader ******\n")
 	fmt.Println("[+] The base_coin is: " + base_coin)
 	fmt.Println("[+] The market_coin is: " + market_coin)
-	fmt.Println("[+] The api_version is: " + api_versio
+	fmt.Println("[+] The api_version is: " + api_version)
+	
